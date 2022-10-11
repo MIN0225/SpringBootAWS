340310df47096fef492365d59531b9446709c805
GM <cro77@naver.com>
2022-10-12 02:17:22 +0900

    3.3 까지 진행 Spring Data Jpa 적용하기 Spring Data JPA 테스트 코드 작성

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -3,9 +3,9 @@ package com.jojoldu.book.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // 스프링 부트 자동설정, 스프링 Bean 읽기 생성 모두 자동으로 설정
 public class Application {
     public static void main(String[] args){
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);// 내장 WAS 실행
     }
 }
