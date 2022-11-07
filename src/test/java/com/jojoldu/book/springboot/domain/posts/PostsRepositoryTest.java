c39929957c9388a28693f9fbf3ae3c36c8de69bf
GM <cro77@naver.com>
2022-11-07 23:31:38 +0900

    4.3 게시글 등록 버튼 누르면 오류 발생...

--- src/test/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -62,6 +62,5 @@ public class PostsRepositoryTest {
         System.out.println(">>>>>>>> createDate=" + posts.getCreatedDate()+", modifiedDate="+posts.getModifiedDate());
         assertThat(posts.getCreatedDate()).isAfter(now);
         assertThat(posts.getModifiedDate()).isAfter(now);
-
     }
 }
