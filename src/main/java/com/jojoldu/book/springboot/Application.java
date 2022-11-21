f2696e46aa7a612d5f208142465fe2b7779815f8
GM <cro77@naver.com>
2022-11-21 22:28:09 +0900

    HelloResponseDtoTest assertThat 주석추가

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+// @EnableJpaAuditing 삭제
 @SpringBootApplication // 스프링 부트 자동설정, 스프링 Bean 읽기 생성 모두 자동으로 설정
 public class Application {
     public static void main(String[] args){
