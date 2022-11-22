16bf3f69da90b57e42fadf53fada4056eb7bc7fd
GM <cro77@naver.com>
2022-11-22 19:13:58 +0900

    HelloController @RequestParam 주석추가

--- src/main/java/com/jojoldu/book/springboot/web/HelloController.java
+++ src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -14,7 +14,7 @@ public class HelloController {
 
     @GetMapping("/hello/dto")
     public HelloResponseDto helloDto(@RequestParam("name") String
-                                                 name,
+                                                 name, // @RequestParam 외부에서 API로 넘긴 파라미터를 가져오는 어노테이션입니다. 여기서는 외부에서 name (@RequestParam("name"))이란 이름으로 넘긴 파라미터를 메소드 파라미터 name(String name)에 저장하게 된다.
                                      @RequestParam("amount") int
                                              amount){
         return new HelloResponseDto(name, amount);
